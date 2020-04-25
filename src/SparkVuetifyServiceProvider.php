e05fabcf7db730c6531591fdb6d36c424adfe05c
Kyle Hurst <kyle.c.hurst@gmail.com>
2020-04-25 02:27:55 -0400

    First commit

---
+++ src/SparkVuetifyServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Corbin\SparkVuetify;
+
+class SparkVuetifyServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
