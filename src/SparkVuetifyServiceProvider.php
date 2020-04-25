fa9c4bc1283091ae8e1ece4f16dfcb708bc3c283
Kyle Hurst <kyle.c.hurst@gmail.com>
2020-04-25 02:49:50 -0400

    Add base spark view templates

--- src/SparkVuetifyServiceProvider.php
+++ src/SparkVuetifyServiceProvider.php
@@ -6,7 +6,7 @@ class SparkVuetifyServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
+        $this->loadViewsFrom(__DIR__.'/path/to/views', 'spark-vuetify');
     }
 
     public function register()
